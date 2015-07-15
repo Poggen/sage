6897db2eb12e31369b57deeb514aff51fab119f4
Oscar Hillestad <oscarhillestad@gmail.com>
2015-07-15 12:54:04 +0200

    Added MDL markup

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,8 @@
-<footer class="content-info" role="contentinfo">
-  <div class="container">
+<footer class="mdl-mega-footer" role="contentinfo">
+  <div class="mdl-mega-footer--top-section">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+  </div>
+  <div class="mdl-mega-footer--bottom-section">
+    <a class="mdl-logo" href="<?= esc_url(home_url('/')); ?>">&copy; <?php echo date("Y") ?> <?php bloginfo('name'); ?></a> 
   </div>
 </footer>
